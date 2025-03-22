be3f7218984dfbc0e523acf429a87fd9babe2295
Usuario <CORREO>
2025-03-22 11:54:02 +0100

    Recueracion

---
+++ Simulacro del examen de recuperación 3.py
@@ -0,0 +1,8 @@
+# 3. El nombre escrito por el usuario es buscado con un bucle en la lista
+# - Si el nombre está en la lista, "{nombre_buscar} encontrado"
+# - Si el nombre no está en la lista, "{nombre_buscar} no encontrado"
+if nombre_buscar in nombres:
+    print(f"{nombre_buscar} encontrado")
+else:
+    print(f"{nombre_buscar} no encontrado")
+
---
+++ Simulacro del examen de recuperación 4.py
@@ -0,0 +1,7 @@
+# 4. Crea un diccionario, cuya clave sea el nombre de cada uno, y el valor su edad (te la inventas)
+edades = {
+    "Fulano": 30,
+    "Mengano": 25,
+    "Zutano": 20
+}
+
