5b177a6b096c12c35b3b0b8e848f28fb3a9f2491
Usuario <CORREO>
2025-03-22 11:54:56 +0100

    Recueracion

--- Simulacro del examen de recuperación 3.py
+++ Simulacro del examen de recuperación 3.py
@@ -1,8 +1,8 @@
 # 3. El nombre escrito por el usuario es buscado con un bucle en la lista
 # - Si el nombre está en la lista, "{nombre_buscar} encontrado"
 # - Si el nombre no está en la lista, "{nombre_buscar} no encontrado"
-if nombre_buscar in nombres:
-    print(f"{nombre_buscar} encontrado")
+if nombre_buscar in nombres: # type: ignore
+    print(f"{nombre_buscar} encontrado") # type: ignore
 else:
-    print(f"{nombre_buscar} no encontrado")
+    print(f"{nombre_buscar} no encontrado") # type: ignore
 
--- Simulacro del examen de recuperación 5.py
+++ Simulacro del examen de recuperación 5.py
@@ -1,4 +1,4 @@
 # 5. Muestra por pantalla la edad del último elemento de la lista
 # IMPORTANTE: desconocemos la cantidad, por lo que no es correcto utilizar el 2 para Zutano
-ultimo_nombre = nombres[-1]
-print(f"La edad de {ultimo_nombre} es {edades[ultimo_nombre]}")
+ultimo_nombre = nombres[-1] # type: ignore
+print(f"La edad de {ultimo_nombre} es {edades[ultimo_nombre]}") # type: ignore
